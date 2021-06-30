212f21ad85227eecb5efba71715735abd2818291
zhengjinhan <jinhan.zheng@inmeijia.com>
2021-06-30 19:26:54 +0800

    feat: dingtest

--- controllers/api.js
+++ controllers/api.js
@@ -4,11 +4,58 @@ const cwd = process.cwd();
 
 const { exec } = require('child_process');
 
+const crypto = require('crypto');
+
 var gid = 0;
 
 function nextId() {
     gid ++;
     return 't' + gid;
+}
+
+// function makeEncrypt(corpId){
+//     const msg_len = Buffer.from([0,0,0,7])//success 长度为7,二进制为00000000 00000000 00000000 00000111
+//     const random = crypto.randomBytes(16)//16字节的随机字符，可以不是ascii
+//     const msg = Buffer.from('success','ascii')
+//     const corpid = Buffer.from(corpId,'ascii')
+//     const codeStringBuffer = Buffer.concat([random,msg_len,msg,corpid])
+//     const key = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+//     const iv = key.slice(0,16)
+//     const cipher = crypto.createCipheriv('aes-256-cbc',key,iv)
+//     let encrypted = cipher.update(codeStringBuffer,'binary','base64')
+//     encrypted += cipher.final('base64');
+//     return encrypted
+// }
+
+/**
+ * 
+ * @param {*} timeStamp 当前10位的时间戳
+ * @param {*} nonce 随机字符串，长度不限
+ * @param {*} encrypt 新随机字符串+二进制（0007）+ success + CorpId 然后用aes-256-cbc加密，再转为Base64字符串
+ * @param {*} token 注册回调接口时设定的自定义token
+ */
+// function signMsg(timeStamp,nonce,encrypt,token){
+//     let sortList = [timeStamp,nonce,encrypt,token];
+//     sortList.sort();
+//     let msg_signature = '';
+//     for (let text of  sortList){
+//         msg_signature += text;
+//     }
+//     const hash = crypto.createHash('sha1')
+//     hash.update(msg_signature)
+//     msg_signature = hash.digest('hex')
+//     return msg_signature
+// }
+
+function aesDecrypt(encrypted) {
+    const key = 'UCoRKfN6MRrBgizb2bAUU5UMdhDTnIio7dF6b6dbItx'
+    const iv = key.slice(0,16)
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    decipher.setAutoPadding(false)//如果不加这个在解密钉钉加密信息的时候final()容易出错
+    let decrypted= decipher.update(encrypted, 'base64')
+    decrypted= Buffer.concat([decrypted,decipher.final()])
+    const content_length = decrypted.slice(16,20).readInt32BE()//正文的长度，是4个字节的整数
+	return decrypted.slice(20,20+content_length).toString('utf-8')//通过指定长度提取出json
 }
 
 const model = require('../model');
@@ -100,11 +147,26 @@ module.exports = {
     },
 
     'POST /api/dingtest': async (ctx, next) => {
-        console.log(ctx.query, 'request-query');
-        console.log(ctx.request.body, 'request-body');
-        ctx.rest({
-            code: 200,
-            module: '成功'
-        });
+
+        const { timestamp, nonce, msg_signature } = ctx.query;
+        const { encrypt } = ctx.request.body;
+        
+        //下面是返回给钉钉success
+        // const encrypt = makeEncrypt('dingfcaaf41f6d550a24acaaa37764f94726')
+        // const timeStamp = "" + parseInt(timestamp / 1000);
+        // const token = 'svEkzca';
+        // const msg_signature = signMsg(timeStamp,nonce,encrypt,token)
+        const resp = {
+            msg_signature,
+            timestamp,
+            nonce,
+            encrypt
+        }
+        console.log('返回给钉钉的响应是：',resp)
+        res.rest(resp)
+    
+        //下面是解密钉钉推送来的消息
+        const msgFromDing = aesDecrypt(req.body.encrypt)
+        console.log('钉钉发来的消息是：'+msgFromDing)
     },
 }

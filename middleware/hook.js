8cc0fbf82bc008e8e06931f208bd88a6a4f262b8
zhengjinhan <jinhan.zheng@inmeijia.com>
2021-06-30 23:29:24 +0800

    feat: dingtest

--- middleware/hook.js
+++ middleware/hook.js
@@ -1,12 +1,11 @@
 module.exports = async function (ctx, next) {
   const req = ctx.request.body;
   console.log(req.ref, 'req.ref');
-  if ((req.ref || '').includes('heads/master')) {
-    // ctx.rest({
-    //   code: 200,
-    //   module: '忽略'
-    // });
-    await next();
+  if (req.ref && !req.ref.includes('heads/master')) {
+    ctx.rest({
+      code: 200,
+      module: '忽略'
+    });
   } else {
     await next();
   }
